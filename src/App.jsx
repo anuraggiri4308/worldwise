57a9cc6ca24d6271346980502dcaeab9f2f468e3
yadav27nitish <yadav27nitish@gmail.com>
2024-07-13 02:37:29 +0530

    nested route added

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,20 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />}></Route>
+        {/* <Route path="/" element={<Homepage />}></Route> */}
+        {/* Below instead of path="/" we have written index so it will be routed automatically */}
+        <Route index element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="app" element={<AppLayout />}>
+          {/* Below is the default children route, when none children routes matches this will be shown */}
+          <Route index element={<p> Default children routes</p>}></Route>
+
+          {/* Below are three children routes */}
+          <Route path="cities" element={<p>List of cities</p>}></Route>
+          <Route path="countries" element={<p>List of countries</p>}></Route>
+          <Route path="form" element={<p>Form</p>}></Route>
+        </Route>
         <Route path="login" element={<Login />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
